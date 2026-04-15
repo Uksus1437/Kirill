23c2f46cabbfd2286d8f051c8160026a550ea08a
grill1fbb <rost1ck7227@gmail.com>
2026-04-15 10:12:07 +0300

    Update main.py

--- personal/main.py
+++ personal/main.py
@@ -6,3 +6,15 @@ def f(n,k):
     if n<k:
         return f(n+1,k)+f(n+3,k)+f(n*2,k)
 print(f(2,15)*f(15,25))
+
+#number 5
+for n in range(1,300):
+    r=(bin(n))[2:]
+    sm=(r.count('1'))%2
+    r=r+str(sm)
+    sm=(r.count('1'))%2
+    r=r+str(sm)
+    r=int(r,2)
+    if r>253:
+        print(n)
+
